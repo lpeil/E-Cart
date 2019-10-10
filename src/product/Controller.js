1c70e8a1a086a8be529e148690a12cfe8144183b
Luan Peil <46160571+lpeil@users.noreply.github.com>
2019-10-10 08:28:28 -0300

    Add Product Update

--- src/product/Controller.js
+++ src/product/Controller.js
@@ -68,7 +68,7 @@ exports.create = (req, res) => {
 
 exports.remove = (req, res) => {
     let product = req.product;
-    product.remove((err, deletedProduct) => {
+    product.remove((err) => {
         if(err) {
             return res.status(400).json({
                 error: errorHandler(err)
@@ -78,5 +78,49 @@ exports.remove = (req, res) => {
         res.json({
             "message": "Product deleted successfuly"    
         })
+    });
+}
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if(err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            })
+        }
+
+        const { name, description, price, category, quantity, shipping } = fields;
+
+        if(!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: 'All field required'
+            })
+        }
+
+        let product = req.product;
+        product = _.extend(product, fields);
+
+        if(files.photo) {
+            if(files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: 'Image should be less than 1 mb size'
+                })
+            }
+
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type;
+            
+        }
+
+        product.save((err, result) => {
+            if(err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            return res.json(result);
+        });
     });
 }
