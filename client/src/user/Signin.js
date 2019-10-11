4c2804a3bc6c8b24d072a06b5b510758ab779899
lpeil <luan@peil.dev>
2019-10-11 09:40:45 -0300

    React Fix

---
+++ client/src/user/Signin.js
@@ -0,0 +1,7 @@
+import React from React;
+
+const Signin = () => (
+    <div>Signin</div>
+);
+
+export default Signin;
---
+++ client/src/user/Signup.js
@@ -0,0 +1,7 @@
+import React from React;
+
+const Signup = () => (
+    <div>Signup</div>
+);
+
+export default Signup;
