f7d1d0175873fc36660b3c8b9dbb890dc2f09090
Luan Peil <luan@peil.dev>
2019-10-10 22:10:27 -0300

    Add Products by Search

--- src/product/Controller.js
+++ src/product/Controller.js
@@ -163,3 +163,53 @@ exports.listRelated = (req, res) => {
         res.json(data);
     })
 }
+
+exports.listCategories = (req, res) => {
+    Product.distinct("category", {}, (err, data) => {
+        if(err) {
+            return res.status(400).json({
+                error: 'Categories not found'
+            })
+        }
+        res.json(data);
+    })
+}
+
+exports.listBySearch = (req, res) => {
+    let order = req.body.order ? req.body.order : "desc";
+    let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+    let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+    let skip = parseInt(req.body.skip);
+    let findArgs = {};
+ 
+    for (let key in req.body.filters) {
+        if (req.body.filters[key].length > 0) {
+            if (key === "price") {
+                findArgs[key] = {
+                    $gte: req.body.filters[key][0],
+                    $lte: req.body.filters[key][1]
+                };
+            } else {
+                findArgs[key] = req.body.filters[key];
+            }
+        }
+    }
+ 
+    Product.find(findArgs)
+        .select("-photo")
+        .populate("category")
+        .sort([[sortBy, order]])
+        .skip(skip)
+        .limit(limit)
+        .exec((err, data) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "Products not found"
+                });
+            }
+            res.json({
+                size: data.length,
+                data
+            });
+        });
+};
--- src/product/Routes.js
+++ src/product/Routes.js
@@ -1,7 +1,17 @@
 const { Router } = require('express');
 const router = Router();
 
-const { create, productById, read, remove, update, list, listRelated } = require('./Controller');
+const { 
+    create, 
+    productById, 
+    read, 
+    remove, 
+    update, 
+    list, 
+    listRelated, 
+    listCategories, 
+    listBySearch 
+} = require('./Controller');
 const { requireSignin, isAdmin, isAuth } = require('../auth/Controller');
 const { userById } = require('../user/Controller');
 
@@ -11,6 +21,8 @@ router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update
 router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove);
 router.get('/products', list);
 router.get('/products/related/:productId', listRelated);
+router.get('/products/categories', listCategories);
+router.post("/products/by/search", listBySearch);
 
 router.param('userId', userById);
 router.param('productId', productById);
